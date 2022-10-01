7b2d13952c25570f0d14015353c04524e3eabb78
FilipeSouza <filipe.souza1602@gmail.com>
2022-10-01 15:13:59 -0300

    feat: create initial static interface of avatarUpload component

--- src/components/AvatarUpload/avatarUpload.tsx
+++ src/components/AvatarUpload/avatarUpload.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import './avatarUploadStyles.scss'
+import { FaMountain } from 'react-icons/fa'
 
 const AvatarUpload = () => {
   return (
     <div className='box'>
-      Avatar Upload
+      <div className='drop-file-container'>
+        <span className='file-title'>
+          <FaMountain/> 
+          <span>Organization Logo</span>
+        </span>
+        <span className='file-subtitle'>
+          Drop the image here or click to browse.
+        </span>
+      </div>
     </div>
   )
 }
