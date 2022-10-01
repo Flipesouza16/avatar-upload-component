b443aec42e8dac20ae68124535d3cb3b7297d8c7
FilipeSouza <filipe.souza1602@gmail.com>
2022-10-01 20:36:41 -0300

    feat: display image on drag and drop component

--- src/components/AvatarUpload/avatarUpload.tsx
+++ src/components/AvatarUpload/avatarUpload.tsx
@@ -1,21 +1,58 @@
-import React from 'react'
-import './avatarUploadStyles.scss'
-import { FaMountain } from 'react-icons/fa'
+import React, { useCallback, useState } from "react";
+import { useDropzone } from "react-dropzone";
+import { FaMountain } from "react-icons/fa";
+import "./avatarUploadStyles.scss";
 
-const AvatarUpload = () => {
+type ImageModel = {
+  path: string;
+  preview: string;
+  lastModified: string;
+  lastModifiedDate: string;
+  name: string;
+  size: string;
+  type: string;
+  webkitRelativePath: string;
+};
+
+const AvatarUpload: React.FC = () => {
+  const [selectedImage, setSelectedImage] = useState<ImageModel>();
+
+  const onDrop = useCallback((acceptedFiles: any[]) => {
+    acceptedFiles.forEach((file) => {
+      const newImage: ImageModel = Object.assign(file, {
+        preview: URL.createObjectURL(file),
+      });
+
+      setSelectedImage(newImage);
+    });
+  }, []);
+
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
+
   return (
-    <div className='box'>
-      <div className='drop-file-container'>
-        <span className='file-title'>
-          <FaMountain/> 
-          <span>Organization Logo</span>
-        </span>
-        <span className='file-subtitle'>
-          Drop the image here or click to browse.
-        </span>
+    <div className="box" {...getRootProps()}>
+      <input type="file" {...getInputProps()} />
+      <div className="drop-file-container">
+        <div className='container-info-image'>
+          {selectedImage && (
+            <img src={selectedImage.preview} className="file-image" alt="preview"></img>
+          )}
+        
+          <div className={`container-titles ${selectedImage && 'container-title-with-image-filled'}`}>
+            <span className="file-title">
+              <FaMountain />
+              <span>Organization Logo</span>
+            </span>
+
+            <span className="file-subtitle">
+              Drop the image here or click to browse.
+            </span>
+          </div>
+        </div>
+
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AvatarUpload
+export default AvatarUpload;
