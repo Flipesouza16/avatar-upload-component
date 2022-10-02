9de6b1883a84e35eb749c4e5b09de34a6a067275
FilipeSouza <filipe.souza1602@gmail.com>
2022-10-02 06:32:02 -0300

    feat: save selected image

--- src/components/AvatarUpload/avatarUpload.tsx
+++ src/components/AvatarUpload/avatarUpload.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState, useEffect } from "react";
+import React, { useCallback, useState } from "react";
 import { useDropzone } from "react-dropzone";
 import { FaMountain } from "react-icons/fa";
 import { GrClose } from "react-icons/gr";
@@ -10,20 +10,21 @@ type ImageModel = File & {
   preview?: string;
   name?: string;
   type?: string;
+  zoomValue?: number;
 };
 
 type AcceptedFilesModel = (args: ImageModel[]) => void;
 
 const AvatarUpload: React.FC = () => {
   const [selectedImage, setSelectedImage] = useState<ImageModel | null>();
-  const [imageValue, setImageValue] = useState<number>(1);
-
+  const [zoomImageValue, setZoomImageValue] = useState<number>(1);
   const [isSelectedImageSaved, setIsSelectedImageSaved] =
     useState<boolean>(false);
-
   const [isErroUpload, setIsErroUpload] = useState<boolean>(false);
 
   const onDrop = useCallback<AcceptedFilesModel>((acceptedFiles) => {
+    returnToInitialState()
+    
     acceptedFiles.forEach((file) => {
       if (!file.type.includes("image")) {
         setIsErroUpload(true);
@@ -44,44 +45,56 @@ const AvatarUpload: React.FC = () => {
   const returnToInitialState = (): void => {
     setSelectedImage(null);
     setIsErroUpload(false);
+    setIsSelectedImageSaved(false);
+    setZoomImageValue(1)
   };
 
-  const isDisplayBoxDashed = (): boolean => {
+  const isInitialState = (): boolean => {
     return (
       !selectedImage || (selectedImage && !isErroUpload && isSelectedImageSaved)
     );
   };
 
-  const getBackgroundSize = () => {
-    return { backgroundSize: `${(imageValue * 100) / 10}%` }
-  }
+  const updateZoomImageValue = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const newValue = e.target.valueAsNumber;
+    if (newValue >= 1) {
+      setZoomImageValue(newValue);
+    }
+  };
 
-  const updateImageValue = (e: React.ChangeEvent<HTMLInputElement>) => {
-    e.stopPropagation()
-    const newValue = e.target.valueAsNumber
-    setImageValue(newValue)
-  }
+  const saveImage = (): void => {
+    if (selectedImage) {
+      const imageToSave = selectedImage;
+      imageToSave.zoomValue = zoomImageValue;
+      setIsSelectedImageSaved(true);
+      setSelectedImage(imageToSave);
+    }
+  };
 
   return (
-    <div className={`box ${isDisplayBoxDashed() && "box-dashed"}`}>
-      <div className="box-header">
-        <GrClose size={20} onClick={returnToInitialState} />
-      </div>
+    <div className={`box ${isInitialState() && "box-dashed"}`}>
+      {!isInitialState() && (
+        <div className="box-header">
+          <GrClose size={20} onClick={returnToInitialState} />
+        </div>
+      )}
       <div className="drop-file-container">
         <div className="container-info-image">
           {selectedImage &&
             (isErroUpload ? (
-              <div className="container-image container-image-error" {...getRootProps()}>
+              <div
+                className="container-image container-image-error"
+              >
                 <div className="file-image-icone">
                   <RiErrorWarningFill className="icone" />
                 </div>
               </div>
             ) : (
-              <div className="container-image" {...getRootProps()}>
+              <div className="container-image">
                 <img
                   src={selectedImage.preview}
                   className="file-image"
-                  style={{ transform: `scale(${imageValue})` }}
+                  style={{ transform: `scale(${zoomImageValue})` }}
                   alt="preview"
                 ></img>
               </div>
@@ -97,24 +110,34 @@ const AvatarUpload: React.FC = () => {
                 <span className="file-title title-error" {...getRootProps()}>
                   Sorry, the upload failed.
                 </span>
-                <span className="file-subtitle subtitle-error" {...getRootProps()}>Try again</span>
+                <span
+                  className="file-subtitle subtitle-error"
+                  {...getRootProps()}
+                >
+                  Try again
+                </span>
               </>
             ) : (
               <>
                 {selectedImage && !isSelectedImageSaved ? (
                   <div className="container-image-to-save">
                     <span>Crop</span>
-                    <input 
-                      type="range" 
-                      max={10} 
-                      value={imageValue}
-                      onChange={updateImageValue} 
-                      style={getBackgroundSize()} 
+                    <input
+                      type="range"
+                      max={10}
+                      value={zoomImageValue}
+                      onChange={updateZoomImageValue}
                     />
+                    <button className="button-save" onClick={saveImage}>
+                      Save
+                    </button>
                   </div>
                 ) : (
                   <>
-                    <span className="file-title title-default" {...getRootProps()}>
+                    <span
+                      className="file-title title-default"
+                      {...getRootProps()}
+                    >
                       <FaMountain />
                       <span>Organization Logo</span>
                     </span>
